82dbf0b6af7fc357b97a55c2617141ea4c07d8bf
HDR131 <haynesrosson2@gmail.com>
2024-01-29 17:58:42 -0600

    Timer screen is finalized. Next steps are to work on the buzzer, powered by battery and commanded by transistor and DAC off board.

--- src/main.cpp
+++ src/main.cpp
@@ -57,18 +57,6 @@ extern volatile bool tmt2flag = true; // small right tomato direction flag
 portMUX_TYPE timerMux0 = portMUX_INITIALIZER_UNLOCKED;
 portMUX_TYPE timerMux1 = portMUX_INITIALIZER_UNLOCKED;
 
-////////////////////////////////////////////////////////////////////////////////////////
-
-void setup() {
-  Serial.begin(115200);
-  // initalizations 
-  amoled.beginAMOLED_191(); // begin screen
-  spr.createSprite(WIDTH, HEIGHT); // create whole screen sprite 
-  spr.setSwapBytes(1);
-  unsigned int seed = analogReadMilliVolts(PIN_D0); // get a random seed 
-  srand(seed); // set random seed 
-
-}
 
 //////////////////////////////////// Timer Counter ////////////////////////////////////
 void IRAM_ATTR timer0Interrupt() {
@@ -140,11 +128,6 @@ void Timer_INIT() {
   int ticks_per_pixel_adj = 100000;
   timerAlarmWrite(Timer1, ticks_per_pixel_adj, true);
   
-  // // timer restart and enable 
-  timerRestart(Timer0);
-  timerRestart(Timer1);
-  timerAlarmEnable(Timer0);
-  timerAlarmEnable(Timer1);
 }
 
 //////////////////////////// Work Timer Screen Function ////////////////////////////
@@ -168,8 +151,11 @@ void DrawTimerWork() {
   // redeclare starting seconds variable for 25 minute countown
   seconds = 25 * 60; 
 
-  // Begin Timers 
-  Timer_INIT();
+  // Begin Timers, timer restart and enable 
+  timerRestart(Timer0);
+  timerRestart(Timer1);
+  timerAlarmEnable(Timer0);
+  timerAlarmEnable(Timer1);
 
   // work period countdown timer loop 
   while (seconds >= 0) {
@@ -253,8 +239,11 @@ void DrawTimer5Break() {
   zzzsml.pushImage(0, 0, 32, 32, zzz_img);
 
 
-  // Begin Timers 
-  Timer_INIT();
+  // Begin Timers, timer restart and enable 
+  timerRestart(Timer0);
+  timerRestart(Timer1);
+  timerAlarmEnable(Timer0);
+  timerAlarmEnable(Timer1);
 
   // 5 Min Break Period countdown timer loop 
   while (seconds >= 0) {
@@ -314,18 +303,122 @@ void DrawTimer5Break() {
   timerAlarmDisable(Timer0);
 }
 
+////////////////////////////////////////////////////////////////////////////////////////
+
+///////////////////////// 15 MinuteBreak Timer Screen Function /////////////////////////
+
+void DrawTimer15Break() {
+
+  // redefine starting seconds varibale for countdown 
+  seconds = 15 * 60;
+
+  // create stopwatch sprite 
+  stpwtch.createSprite(64,64);
+  stpwtch.setSwapBytes(true);
+  stpwtch.pushImage(0, 0, 64, 64, stopwatch_img);
+
+  // create large break image sprite 
+  brklrg.createSprite(128,128);
+  brklrg.setSwapBytes(false);
+  brklrg.pushImage(0, 0, 128, 128, break_img);
+
+  // create zzz image sprite 
+  zzzsml.createSprite(32, 32);
+  zzzsml.setSwapBytes(false);
+  zzzsml.pushImage(0, 0, 32, 32, zzz_img);
 
 
+  // Begin Timers, timer restart and enable 
+  timerRestart(Timer0);
+  timerRestart(Timer1);
+  timerAlarmEnable(Timer0);
+  timerAlarmEnable(Timer1);
 
+  // 5 Min Break Period countdown timer loop 
+  while (seconds >= 0) {
 
+    // Draw background 
+    spr.fillRect(0, 0, WIDTH, HEIGHT, color6);
+    spr.fillCircle(20, 100, 95, color7);
+    spr.fillCircle(340, 190, 150, color8);
 
+    // create the break period lable string/sprite
+    spr.setTextColor(color3);
+    String wrktxt = "...Break Period...";
+    spr.drawString(wrktxt, 200, 25, 4);
+
+    // create timer outline round rectangles 
+    spr.fillRoundRect(85, 85, 310, 135, 25, color3);
+    spr.fillRoundRect(90, 90, 300, 125, 18, color7);
+
+    // convert second and minute values 
+    int s = seconds % 60;
+    int m = (seconds % 3600) / 60;
+    int s_size = std::to_string(s).length();
+
+    // create timer number string for minutes 
+    String m_str = String(m);
+    // if statment to keep alignment if single digit minutes 
+    if (m_str.length() == 1) {
+      m_str = "0" + m_str; 
+    }
+    // create timer number string for seconds 
+    String s_str = String(s); 
+    // if statment to keep alignment if single digits seconds 
+    if (s_str.length() == 1) {
+      s_str = "0" + s_str;
+    }
+    // create total time string 
+    String currTime = m_str + ":" + s_str; 
+    spr.setTextColor(color3, color7);
+    spr.drawString(currTime, 115, 110, 8);
+
+    // Push stopwatch sprite to &spr 
+    stpwtch.pushToSprite(&spr, 10, 120, TFT_BLACK);
+
+    // Push large break sprite to &spr
+    brklrg.pushToSprite(&spr, 400, img_pos1, TFT_BLACK);
+
+    // push zzz sprite left of "Break Period" to the sprite class &spr 
+    zzzsml.pushToSprite(&spr, 165, img_pos2, TFT_BLACK);
+
+    // push zzz sprite right of "Break Period" to &spr
+    zzzsml.pushToSprite(&spr, 390, img_pos3, TFT_BLACK);
+
+    // push all drawn sprites to the screen
+    amoled.pushColors(0, 0, WIDTH, HEIGHT, (uint16_t *)spr.getPointer());
+  }
+
+  timerAlarmDisable(Timer0);
+}
+
+////////////////////////////////////////////////////////////////////////////////////////
+
+void setup() {
+  Serial.begin(115200);
+  // initalizations 
+  amoled.beginAMOLED_191(); // begin screen
+  spr.createSprite(WIDTH, HEIGHT); // create whole screen sprite 
+  spr.setSwapBytes(1);
+  unsigned int seed = analogReadMilliVolts(PIN_D0); // get a random seed 
+  srand(seed); // set random seed 
+  Timer_INIT(); // prepare timers 
+
+}
 
 ///////////////////////////////////////////////////////////////////////////////////////
 
 void loop() {
   // put your main code here, to run repeatedly:
-  //DrawTimerWork();
+  // I could make this look more refined with switch statements, but this looks fine and is readable? 
+  DrawTimerWork();
   DrawTimer5Break();
+  DrawTimerWork();
+  DrawTimer5Break();
+  DrawTimerWork();
+  DrawTimer5Break();
+  DrawTimerWork();
+  DrawTimer15Break();
 }
 
 // put function definitions here:
